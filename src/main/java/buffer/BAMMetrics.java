3ff81b76f4f8536f2f915c1402746db7c64a9d91
brendan <brendan.d.ofallon@aruplab.com>
2013-08-22 10:48:36 -0600

    Check for null value for MQ histogram in QCtoJSON

--- src/main/java/buffer/BAMMetrics.java
+++ src/main/java/buffer/BAMMetrics.java
@@ -44,7 +44,8 @@ public class BAMMetrics extends FileBuffer implements JSONString {
 			obj.put("bases.above.q30", basesQAbove30);
 			obj.put("bases.above.q20", basesQAbove20);
 			obj.put("bases.above.q10", basesQAbove10);
-			obj.put("mq.histo",  mqHistogram.getRawCounts());
+			if (mqHistogram != null)
+				obj.put("mq.histo",  mqHistogram.getRawCounts());
 			if (insertSizeHistogram != null)
 				obj.put("mean.insert.size", insertSizeHistogram.getMean());
 			
