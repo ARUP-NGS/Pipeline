c054ea3f6176c4c45918df076c8681da94620de6
brendan <brendan.d.ofallon@aruplab.com>
2014-06-04 07:36:59 -0600

    Added new SnpEff annotator

--- src/main/java/operator/annovar/Annotator.java
+++ src/main/java/operator/annovar/Annotator.java
@@ -1,11 +1,14 @@
 package operator.annovar;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.logging.Logger;
 
 import operator.OperationFailedException;
 import operator.Operator;
+import operator.StringPipeHandler;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -115,6 +118,50 @@ public abstract class Annotator extends Operator {
 				throw new OperationFailedException("Task was interrupted : " + System.err.toString() + "\n" + e.getLocalizedMessage(), this);
 			}
 
+		}
+		catch (IOException e1) {
+			throw new OperationFailedException("Task encountered an IO exception : " + System.err.toString() + "\n" + e1.getLocalizedMessage(), this);
+		}
+	}
+	
+	
+	protected void executeCommandCaptureOutput(final String command, File outputFile) throws OperationFailedException {
+		Runtime r = Runtime.getRuntime();
+		final Process p;
+
+		try {
+			FileOutputStream outputStream = new FileOutputStream(outputFile);
+			
+			p = r.exec(command);
+			
+			//Weirdly, processes that emits tons of data to their error stream can cause some kind of 
+			//system hang if the data isn't read. Since BWA and samtools both have the potential to do this
+			//we by default capture the error stream here and write it to System.err to avoid hangs. s
+			final Thread errConsumer = new StringPipeHandler(p.getErrorStream(), System.err);
+			errConsumer.start();
+			
+			final Thread outputConsumer = new StringPipeHandler(p.getInputStream(), outputStream);
+			outputConsumer.start();
+			
+			//If runtime is going down, destroy the process so it won't become orphaned
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				public void run() {
+					//System.err.println("Invoking shutdown thread, destroying task with command : " + command);
+					p.destroy();
+					errConsumer.interrupt();
+					outputConsumer.interrupt();
+				}
+			});
+		
+			try {
+				if (p.waitFor() != 0) {
+					throw new OperationFailedException("Task terminated with nonzero exit value : " + System.err.toString() + " command was: " + command, this);
+				}
+			} catch (InterruptedException e) {
+				throw new OperationFailedException("Task was interrupted : " + System.err.toString() + "\n" + e.getLocalizedMessage(), this);
+			}
+
+			outputStream.close();
 		}
 		catch (IOException e1) {
 			throw new OperationFailedException("Task encountered an IO exception : " + System.err.toString() + "\n" + e1.getLocalizedMessage(), this);
