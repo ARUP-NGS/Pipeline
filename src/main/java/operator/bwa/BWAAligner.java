f6448ae566d4b82104efd37843a8b6ebbc4411bb
Daniel Baker <daniel.baker@aruplab.com>
2014-06-18 16:03:13 -0600

    Returning BWA Aligner to original form.

--- src/main/java/operator/bwa/BWAAligner.java
+++ src/main/java/operator/bwa/BWAAligner.java
@@ -1,17 +1,10 @@
 package operator.bwa;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import operator.IOOperator;
-import operator.OperationFailedException;
 import operator.PipedCommandOp;
-import operator.StringPipeHandler;
 import pipeline.Pipeline;
 import pipeline.PipelineXMLConstants;
 
-public class BWAAligner extends IOOperator {
+public class BWAAligner extends PipedCommandOp {
 	
 	public static final String PATH = "path";
 	public static final String THREADS = "threads";
@@ -24,48 +17,28 @@ public class BWAAligner extends IOOperator {
 		return true;
 	}
 	
-	}
-
-	protected void executeCommandCaptureOutput(final String command, File outputFile) throws OperationFailedException {
-		Runtime r = Runtime.getRuntime();
-		final Process p;
-
-		try {
-			FileOutputStream outputStream = new FileOutputStream(outputFile);
-			
-			p = r.exec(command);
-			
-			//Weirdly, processes that emits tons of data to their error stream can cause some kind of 
-			//system hang if the data isn't read. Since BWA and samtools both have the potential to do this
-			//we by default capture the error stream here and write it to System.err to avoid hangs. s
-			final Thread errConsumer = new StringPipeHandler(p.getErrorStream(), System.err);
-			errConsumer.start();
-			
-			final Thread outputConsumer = new StringPipeHandler(p.getInputStream(), outputStream);
-			outputConsumer.start();
-			
-			//If runtime is going down, destroy the process so it won't become orphaned
-			Runtime.getRuntime().addShutdownHook(new Thread() {
-				public void run() {
-					//System.err.println("Invoking shutdown thread, destroying task with command : " + command);
-					p.destroy();
-					errConsumer.interrupt();
-					outputConsumer.interrupt();
-				}
-			});
+	@Override
+	protected String getCommand() {
 		
-			try {
-				if (p.waitFor() != 0) {
-					throw new OperationFailedException("Task terminated with nonzero exit value : " + System.err.toString() + " command was: " + command, this);
-				}
-			} catch (InterruptedException e) {
-				throw new OperationFailedException("Task was interrupted : " + System.err.toString() + "\n" + e.getLocalizedMessage(), this);
-			}
+		Object propsPath = getPipelineProperty(PipelineXMLConstants.BWA_PATH);
+		if (propsPath != null)
+			pathToBWA = propsPath.toString();
+		
+		String bwaPathAttr = properties.get(PATH);
+		if (bwaPathAttr != null) {
+			pathToBWA = bwaPathAttr;
+		}
+		
+		String threadsAttr = properties.get(THREADS);
+		if (threadsAttr != null) {
+			threads = Integer.parseInt(threadsAttr);
+		}
+			
+		String referencePath = inputBuffers.get(0).getAbsolutePath();
+		String readsPath = inputBuffers.get(1).getAbsolutePath();
 
-			outputStream.close();
-		}
-		catch (IOException e1) {
-			throw new OperationFailedException("Task encountered an IO exception : " + System.err.toString() + "\n" + e1.getLocalizedMessage(), this);
-		}
+		String command = pathToBWA + " aln -t " + threads + " " + referencePath + " " + readsPath;
+		return command;
 	}
+
 }
