c61a126b9638d188b87fc6ed6ff0147ab9ff57c6
Daniel Baker <daniel.baker@aruplab.com>
2014-06-19 16:51:23 -0600

    Rewriting operators without the CatchOutput command. It seems to  be trimming some of my fastq entries.

--- src/main/java/operator/bwa/BWAalnD.java
+++ src/main/java/operator/bwa/BWAalnD.java
@@ -16,28 +16,30 @@ import buffer.SAIFile;
  */
 
 public class BWAalnD extends IOOperator {
-	
+
 	public static final String PATH = "path";
 	public static final String THREADS = "threads";
 	protected String pathToBWA = "bwa";
 	protected int defaultThreads = 4;
 	protected int threads = defaultThreads;
 	String defaultBWAPath = "bwa";
+
 	@Override
 	public boolean requiresReference() {
 		return true;
 	}
+
 	@Override
 	public void performOperation() throws OperationFailedException {
 		Object propsPath = getPipelineProperty(PipelineXMLConstants.BWA_PATH);
 		if (propsPath != null)
 			pathToBWA = propsPath.toString();
-		
+
 		String bwaPathAttr = properties.get(PATH);
 		if (bwaPathAttr != null) {
 			pathToBWA = bwaPathAttr;
 		}
-		
+
 		String threadsAttr = properties.get(THREADS);
 		if (threadsAttr != null) {
 			threads = Integer.parseInt(threadsAttr);
@@ -48,11 +50,11 @@ public class BWAalnD extends IOOperator {
 		String referencePath = reference.getAbsolutePath();
 		FileBuffer reads = this.getInputBufferForClass(FastQFile.class);
 		String readsPath = reads.getAbsolutePath();
-		String command_str = pathToBWA + " aln -t " + threads + " " + referencePath + " " + readsPath;
+		String command_str = pathToBWA + " aln -t " + threads + " -f "
+				+ saiPath + " " + referencePath + " " + readsPath;
 		System.out.println(command_str);
-		executeCommandCaptureOutput(command_str, saiPath);
+		executeCommand(command_str);
 
 	}
-
 
 }
