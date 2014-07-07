abe000ea53f9e18b7cef3ebdcc90902011fa5f12
Daniel Baker <daniel.baker@aruplab.com>
2014-07-07 17:37:02 -0600

    New working filter requiring a specific fraction of bp in a read have 'M' as their cigar operator along with an updated template for RNAxLoc which uses it.

---
+++ src/main/java/operator/bamutils/BAMClassifier.java
@@ -0,0 +1,110 @@
+package operator.bamutils;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import net.sf.samtools.SAMFileReader;
+import net.sf.samtools.SAMFileReader.ValidationStringency;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMFileWriterFactory;
+import net.sf.samtools.SAMRecord;
+import operator.IOOperator;
+import operator.OperationFailedException;
+import pipeline.Pipeline;
+import buffer.BAMFile;
+import buffer.FileBuffer;
+
+/**
+ * Base class for operators that read the contents of bam files, do some processing, and emit a new
+ * BAM file for each classification option. Currently only supports pass/fail categorization.
+ * @author daniel
+ *
+ */
+public abstract class BAMClassifier extends IOOperator {
+
+	//Created a class for returning both a return value (e.g., Boolean) and an alignment record
+	//@author daniel
+	public class ReturnRecord {
+		public final boolean returnValue;
+		public final SAMRecord samRecord;
+		public ReturnRecord(SAMRecord samRecord, boolean returnValue) {
+			this.returnValue = returnValue;
+			this.samRecord = samRecord;
+		}
+		
+	}
+	
+	@Override
+	public void performOperation() throws OperationFailedException {
+				
+		Logger.getLogger(Pipeline.primaryLoggerName).info("Initializing BAMClassifier " + getObjectLabel());
+		
+		BAMFile inputBAM = (BAMFile)super.getInputBufferForClass(BAMFile.class);
+		List<FileBuffer> outputBAMs = super.getAllOutputBuffersForClass(BAMFile.class);
+		BAMFile outputBAMpass = (BAMFile)outputBAMs.get(0);
+		BAMFile outputBAMfail = (BAMFile)outputBAMs.get(1);
+		if (inputBAM == null)
+			throw new OperationFailedException("No input BAM file found", this);
+		//TODO: Generalize to any number of output BAMs
+			
+		classifyBAMFile(inputBAM, outputBAMpass, outputBAMfail);
+			
+		
+		Logger.getLogger(Pipeline.primaryLoggerName).info("BAMClassifier " + getObjectLabel() + " has completed");
+	}
+
+	
+	
+	public void classifyBAMFile(BAMFile inputBAM, BAMFile outputBAMpass, BAMFile outputBAMfail) throws OperationFailedException {
+		SAMFileReader.setDefaultValidationStringency(ValidationStringency.LENIENT);
+		if (inputBAM.getFile() == null) {
+			throw new IllegalArgumentException("File associated with inputBAM " + inputBAM.getAbsolutePath() + " is null");
+		}
+		final SAMFileReader inputSam = new SAMFileReader(inputBAM.getFile());
+		inputSam.setValidationStringency(ValidationStringency.LENIENT);
+		
+		SAMFileWriterFactory factory = new SAMFileWriterFactory();
+		final SAMFileWriter writerPass = factory.makeBAMWriter(inputSam.getFileHeader(), false, outputBAMpass.getFile());
+		final SAMFileWriter writerFail = factory.makeBAMWriter(inputSam.getFileHeader(), false, outputBAMfail.getFile());
+		
+		long recordsRead = 0;
+		long recordsPassed = 0;
+		long recordsFailed = 0;
+		long recordsWritten = 0;
+		for (final SAMRecord samRecord : inputSam) {
+			ReturnRecord returnRecord = processRecord(samRecord);
+			SAMRecord outputRecord = returnRecord.samRecord;
+			recordsRead++;
+			
+			if (returnRecord.returnValue == true) {
+				writerPass.addAlignment(outputRecord);
+				recordsWritten++;
+				recordsPassed++;
+			}
+			else if (returnRecord.returnValue == false) {
+				writerFail.addAlignment(outputRecord);
+				recordsWritten++;
+				recordsFailed++;
+			}
+			else {
+				throw new OperationFailedException("This should never happen - requires that a boolean be neither true nor false", this);
+			}
+			
+		}
+		
+		Logger.getLogger(Pipeline.primaryLoggerName).info(getObjectLabel() + " wrote " + recordsWritten + " of " + recordsRead + " from file " + inputBAM.getAbsolutePath());
+		Logger.getLogger(Pipeline.primaryLoggerName).info(getObjectLabel() + " passed " + recordsPassed + " of " + recordsRead + " from file " + inputBAM.getAbsolutePath());
+		Logger.getLogger(Pipeline.primaryLoggerName).info(getObjectLabel() + " failed " + recordsFailed + " of " + recordsRead + " from file " + inputBAM.getAbsolutePath());
+		inputSam.close();
+		writerPass.close();
+		writerFail.close();
+	}
+	
+	/**
+	 * Peform processing of single record from bam file, return record 
+	 * if read should not be in output file. 
+	 * @param samRecord
+	 * @return
+	 */
+	public abstract ReturnRecord processRecord(SAMRecord samRecord);
+}
--- src/main/java/operator/bamutils/FracAlignFilter.java
+++ src/main/java/operator/bamutils/FracAlignFilter.java
@@ -1,66 +1,71 @@
 package operator.bamutils;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import operator.OperationFailedException;
 import net.sf.samtools.Cigar;
 import net.sf.samtools.CigarElement;
+import net.sf.samtools.CigarOperator;
 import net.sf.samtools.SAMRecord;
 
 /**
- * Removes reads with less than a fraction of the read being mapped.
+ * Splits a BAM file into one of two output files - one BAM for "fail" reads
+ * which have less than a given fraction (default: 0.7) mapped to a reference.
+ * 
  * @author daniel
  *
  */
-public class FracAlignFilter extends AbstractBAMFilter {
-	
+public class FracAlignFilter extends BAMClassifier {
 	public static final String FRACTION = "fraction";
-	
-	public FracAlignFilter() throws OperationFailedException{
-		List<ReadFilter> readFilters = new ArrayList<ReadFilter>();
-		readFilters.add(new CigFilter());
-		setFilters(readFilters);
+	public static final float defaultFrac = (float) 0.7;
+
+	public ReturnRecord processRecord(SAMRecord samRecord) {
+		boolean value = readPasses(samRecord); // Initializing return value for
+												// this function.
+		ReturnRecord returnValue = new ReturnRecord(samRecord, value);
+		return returnValue;
 	}
-	
+
 	/**
-	 * Fail read if less than a given fraction is mapped. 
+	 * Fail read if less than a given fraction is mapped.
+	 * 
 	 * @author daniel
 	 *
 	 */
-	class CigFilter implements ReadFilter {
-			
-		@Override
-		public boolean readPasses(SAMRecord read) {
-			float defaultFrac = (float) 0.7;
-			float fraction = defaultFrac;
-			if(getAttribute(FRACTION) != null){
-				fraction = Float.parseFloat(getAttribute(FRACTION));
+
+	public boolean readPasses(SAMRecord read) {
+		float fraction = defaultFrac;
+		if (getAttribute(FRACTION) != null) {
+			fraction = Float.parseFloat(getAttribute(FRACTION));
+		}
+		// If the fraction is not between 0 and 1, set it to the default value.
+		if (fraction < 0 || fraction > 1) {
+			System.out
+					.println("Fraction provided is not in the scope of [0,1]. \n"
+							+ Float.toString(fraction)
+							+ "Fraction is now set to default "
+							+ Float.toString(defaultFrac));
+			fraction = defaultFrac;
+		}
+		int sumMap = 0;
+		//Acquire cigar, sum mapped ("M") bases
+		Cigar cig = read.getCigar();
+		List<CigarElement> cigContents = cig.getCigarElements();
+		for (CigarElement cigEl : cigContents ) {
+			if (cigEl.getOperator() == CigarOperator.M) {
+				sumMap += cigEl.getLength();
 			}
-			//If the fraction is not between 0 and 1, set it to the default value.
-			if(fraction<0 || fraction >1){
-				System.out.println("Fraction provided is not in the scope of [0,1]. \n" + Float.toString(fraction));
-				fraction = defaultFrac;
-			}
-			int sum=0;
-			int sumMap=0;
-			Cigar cig = read.getCigar();
-			List<CigarElement> cigContents=cig.getCigarElements();
-			int numCig = cigContents.size();
-			for (int i=0;i<numCig;i++) {
-				sum+=Integer.parseInt(cigContents.get(i).toString().replaceAll("\\D+",""));
-				if(cigContents.get(i).toString().contains("M")) {
-					sumMap += Integer.parseInt(cigContents.get(i).toString().replaceAll("\\D+",""));
-				}
-					
-			}
-			if((float) sumMap/sum < fraction) {
-				return false;
-			}
-			else
-				return true;
 		}
 		
+		if ((float) sumMap / read.getReadLength() < fraction) {
+			/*System.out.println("Read failed."); 
+			System.out.println("Mapped bp: " + sumMap + ". Total bp: " + read.getReadLength() + ".\n");
+			*/
+			return false;
+		} else
+			/*System.out.println("Read passed.");
+			System.out.println("Mapped bp: " + sumMap + ". Total bp: " + read.getReadLength() + ".\n");
+			*/
+			return true;
 	}
 
 }
