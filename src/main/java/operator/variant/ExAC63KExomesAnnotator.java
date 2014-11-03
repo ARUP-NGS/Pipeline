d6ac3e6b32415163ab508efecce249d7d963e08a
Daniel Baker <daniel.baker@aruplab.com>
2014-11-03 14:26:00 -0700

    Avoiding writing NaN to annotated csv file.

--- src/main/java/operator/variant/ExAC63KExomesAnnotator.java
+++ src/main/java/operator/variant/ExAC63KExomesAnnotator.java
@@ -47,13 +47,15 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String homCountStr = valueForKey(infoToks, "AC_Hom");
 		if(homCountStr != null) {
 				Double freq = Double.parseDouble(homCountStr)/NumCalledAlleles;
-				var.addProperty(VariantRec.EXOMES_63K_AC_HOM, freq);
+				if(!freq.equals(Double.NaN))
+					var.addProperty(VariantRec.EXOMES_63K_AC_HOM, freq);
 		}
 
 		String hetCountStr = valueForKey(infoToks, "AC_Het");
 		if(hetCountStr != null) {
 				Double freq = Double.parseDouble(hetCountStr)/NumCalledAlleles;
-				var.addProperty(VariantRec.EXOMES_63K_AC_HET, freq);
+				if(!freq.equals(Double.NaN))
+					var.addProperty(VariantRec.EXOMES_63K_AC_HET, freq);
 		}
 
 		//African
@@ -63,19 +65,22 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String afrCountStr = valueForKey(infoToks, "AC_AFR");
 		if (afrCountStr != null) {
 			Double freq = Double.parseDouble(afrCountStr)/NumCalledAFR;
-			var.addProperty(VariantRec.EXOMES_63K_AFR_FREQ, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_AFR_FREQ, freq);
 		}
 		
 		String afrHomStr = valueForKey(infoToks, "Hom_AFR");
 		if (afrHomStr != null) {
 			Double freq = Double.parseDouble(afrHomStr)/NumCalledAFR;
-			var.addProperty(VariantRec.EXOMES_63K_AFR_HOM, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_AFR_HOM, freq);
 		}
 		
 		String afrHetStr = valueForKey(infoToks, "Het_AFR");
 		if (afrHetStr != null) {
 			Double freq = Double.parseDouble(afrHetStr)/NumCalledAFR;
-			var.addProperty(VariantRec.EXOMES_63K_AFR_HET, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_AFR_HET, freq);
 		}		
 		
 		//American
@@ -85,19 +90,22 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String amrCountStr = valueForKey(infoToks, "AC_AMR");
 		if (amrCountStr != null) {
 			Double freq = Double.parseDouble(amrCountStr)/NumCalledAMR;
-			var.addProperty(VariantRec.EXOMES_63K_AMR_FREQ, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_AMR_FREQ, freq);
 		}
 		
 		String amrHomStr = valueForKey(infoToks, "Hom_AMR");
 		if (amrHomStr != null) {
 			Double freq = Double.parseDouble(amrHomStr)/NumCalledAMR;
-			var.addProperty(VariantRec.EXOMES_63K_AMR_HOM, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_AMR_HOM, freq);
 		}
 		
 		String amrHetStr = valueForKey(infoToks, "Het_AMR");
 		if (amrHetStr != null) {
 			Double freq = Double.parseDouble(amrHetStr)/NumCalledAMR;
-			var.addProperty(VariantRec.EXOMES_63K_AMR_HET, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_AMR_HET, freq);
 		}
 		
 		//East Asian
@@ -107,19 +115,22 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String easCountStr = valueForKey(infoToks, "AC_EAS");
 		if (easCountStr != null) {
 			Double freq = Double.parseDouble(easCountStr)/NumCalledEAS;
-			var.addProperty(VariantRec.EXOMES_63K_EAS_FREQ, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_EAS_FREQ, freq);
 		}
 		
 		String easHomStr = valueForKey(infoToks, "Hom_EAS");
 		if (easHomStr != null) {
 			Double freq = Double.parseDouble(easHomStr)/NumCalledEAS;
-			var.addProperty(VariantRec.EXOMES_63K_EAS_HOM, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_EAS_HOM, freq);
 		}
 		
 		String easHetStr = valueForKey(infoToks, "Het_EAS");
 		if (easHetStr != null) {
 			Double freq = Double.parseDouble(easHetStr)/NumCalledEAS;
-			var.addProperty(VariantRec.EXOMES_63K_EAS_HET, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_EAS_HET, freq);
 		}
 		
 		//Finnish
@@ -129,19 +140,22 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String finCountStr = valueForKey(infoToks, "AC_FIN");
 		if (finCountStr != null) {
 			Double freq = Double.parseDouble(finCountStr)/NumCalledFIN;
-			var.addProperty(VariantRec.EXOMES_63K_FIN_FREQ, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_FIN_FREQ, freq);
 		}
 		
 		String finHomStr = valueForKey(infoToks, "Hom_FIN");
 		if (finHomStr != null) {
 			Double freq = Double.parseDouble(finHomStr)/NumCalledFIN;
-			var.addProperty(VariantRec.EXOMES_63K_FIN_HOM, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_FIN_HOM, freq);
 		}
 		
 		String finHetStr = valueForKey(infoToks, "Het_FIN");
 		if (finHetStr != null) {
 			Double freq = Double.parseDouble(finHetStr)/NumCalledFIN;
-			var.addProperty(VariantRec.EXOMES_63K_FIN_HET, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_FIN_HET, freq);
 		}
 		
 		//Non-Finnish Europeans
@@ -151,19 +165,22 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String nfeCountStr = valueForKey(infoToks, "AC_NFE");
 		if (nfeCountStr != null) {
 			Double freq = Double.parseDouble(nfeCountStr)/NumCalledNFE;
-			var.addProperty(VariantRec.EXOMES_63K_NFE_FREQ, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_NFE_FREQ, freq);
 		}
 		
 		String nfeHomStr = valueForKey(infoToks, "Hom_NFE");
 		if (nfeHomStr != null) {
 			Double freq = Double.parseDouble(nfeHomStr)/NumCalledNFE;
-			var.addProperty(VariantRec.EXOMES_63K_NFE_HOM, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_NFE_HOM, freq);
 		}
 		
 		String nfeHetStr = valueForKey(infoToks, "Het_NFE");
 		if (nfeHetStr != null) {
 			Double freq = Double.parseDouble(nfeHetStr)/NumCalledNFE;
-			var.addProperty(VariantRec.EXOMES_63K_NFE_HET, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_NFE_HET, freq);
 		}
 	
 		//Other populations
@@ -179,13 +196,15 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String othHomStr = valueForKey(infoToks, "Hom_OTH");
 		if (othHomStr != null) {
 			Double freq = Double.parseDouble(othHomStr)/NumCalledOTH;
-			var.addProperty(VariantRec.EXOMES_63K_OTH_HOM, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_OTH_HOM, freq);
 		}
 		
 		String othHetStr = valueForKey(infoToks, "Het_OTH");
 		if (othHetStr != null) {
 			Double freq = Double.parseDouble(othHetStr)/NumCalledOTH;
-			var.addProperty(VariantRec.EXOMES_63K_OTH_HET, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_OTH_HET, freq);
 		}
 		
 		//South Asian
@@ -195,19 +214,22 @@ public class ExAC63KExomesAnnotator extends AbstractTabixAnnotator {
 		String sasCountStr = valueForKey(infoToks, "AC_SAS");
 		if (sasCountStr != null) {
 			Double freq = Double.parseDouble(sasCountStr)/NumCalledSAS;
-			var.addProperty(VariantRec.EXOMES_63K_SAS_FREQ, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_SAS_FREQ, freq);
 		}
 		
 		String sasHomStr = valueForKey(infoToks, "Hom_SAS");
 		if (sasHomStr != null) {
 			Double freq = Double.parseDouble(sasHomStr)/NumCalledSAS;
-			var.addProperty(VariantRec.EXOMES_63K_SAS_HOM, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_SAS_HOM, freq);
 		}
 		
 		String sasHetStr = valueForKey(infoToks, "Het_SAS");
 		if (sasHetStr != null) {
 			Double freq = Double.parseDouble(sasHetStr)/NumCalledSAS;
-			var.addProperty(VariantRec.EXOMES_63K_SAS_HET, freq);
+			if(!freq.equals(Double.NaN))
+				var.addProperty(VariantRec.EXOMES_63K_SAS_HET, freq);
 		}	
 		
 		
