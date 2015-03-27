fdbc89af06da41d61b3bf3906637f2cf05b4b165
kes1smmn <ke.monk@gmail.com>
2015-03-27 17:13:30 -0600

    Moved dbSNP under the AbstractTabixAnnotator class. Add unit tests and fixed bugs.

---
+++ src/test/java/annotation/TestDBSNP.java
@@ -0,0 +1,16 @@
+package annotation;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by keith simmon on 3/25/15.
+ *
+ * This test validates the DBSNP database, the test was created when the the
+ *
+ *
+ *
+ * @author Keith Simmon
+ *
+ */
+public class TestDBSNP extends TestCase {
+}
