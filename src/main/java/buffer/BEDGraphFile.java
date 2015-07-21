297992f16989d04af4cb52a541e903d5cb3cddd5
Brendan <brendan.ofallon@aruplab.com>
2015-07-21 13:06:48 -0600

    Add BEDGraph filebuffer class

---
+++ src/main/java/buffer/BEDGraphFile.java
@@ -0,0 +1,25 @@
+package buffer;
+
+import java.io.File;
+
+/**
+ * A BEDGRAPH file is just like a BED file but has a few extra columns. 
+ * @author brendan
+ *
+ */
+public class BEDGraphFile extends BEDFile {
+
+
+	public BEDGraphFile(File file) {
+		super(file);
+		if (! file.getName().endsWith("bedgraph")) {
+			throw new IllegalArgumentException("BEDGraph files must have the suffix .bedgraph");
+		}
+	}
+	
+	@Override
+	public String getTypeStr() {
+		return "BEDGraphFile";
+	}
+	
+}
