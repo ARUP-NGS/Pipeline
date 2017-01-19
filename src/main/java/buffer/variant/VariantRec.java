13f1dba14e0d12bed6e7f62b999de69b58ea9e7f
Jacob Durtschi <331-DurtschiJLPTP@331-DurtschiJ-LPTP-Pro.local>
2017-01-19 08:57:51 -0700

    PE-91 #comment fixed error where snpeff.all annotation not writen to json file

--- src/main/java/buffer/variant/VariantRec.java
+++ src/main/java/buffer/variant/VariantRec.java
@@ -355,6 +355,22 @@ public class VariantRec {
 		return annotations.keySet();
 	}
 	
+	/**
+	 * Collection of all keys used for ints
+	 * @return
+	 */
+	public Collection<String> getIntKeys() {
+		return ints.keySet();
+	}
+	
+	/**
+	 * Collection of all keys used for json annotations
+	 * @return
+	 */
+	public Collection<String> getJsonobjKeys() {
+		return jsonobj.keySet();
+	}
+	
 	public String getAnnotation(String key) {
 		return annotations.get(key);
 	}
