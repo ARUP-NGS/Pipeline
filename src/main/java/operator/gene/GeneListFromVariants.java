983727ed9d6f4ffa1f5e831e4dedf3ee2091db17
LuobinY <yangluob@isu.edu>
2017-03-21 13:44:51 -0600

    Revert GeneListFromVariants.java back; change the version number to be 1.9.9

--- src/main/java/operator/gene/GeneListFromVariants.java
+++ src/main/java/operator/gene/GeneListFromVariants.java
@@ -1,7 +1,6 @@
 package operator.gene;
 
 import java.util.logging.Logger;
-import java.util.logging.Level;
 
 import operator.OperationFailedException;
 import operator.Operator;
@@ -16,10 +15,6 @@ import buffer.GeneList;
 import buffer.variant.VariantPool;
 import buffer.variant.VariantRec;
 
-import json.JSONArray;
-import json.JSONException;
-import json.JSONObject;
-
 /**
  * Create a gene list for every gene found in the variant pool. This also 
  * @author brendan
@@ -29,35 +24,24 @@ public  class GeneListFromVariants extends Operator {
 	
 	VariantPool vars = null;
 	GeneList genes = null;
-        
 	
 	@Override
 	public void performOperation() throws OperationFailedException {
-                Logger logger = Logger.getLogger(Pipeline.primaryLoggerName);
-    	        for(String contig : vars.getContigs()) {
+	
+		
+		for(String contig : vars.getContigs()) {
 			for(VariantRec var : vars.getVariantsForContig(contig)) {
-                                try {
-                                    JSONArray snpeff_annos = var.getjsonProperty(VariantRec.SNPEFF_ALL);
-                                    String geneName = null;
-                                    for(int i=0; i<snpeff_annos.length(); i++) {
-                                        JSONObject jobj = (JSONObject)snpeff_annos.get(i); 
-                                        geneName = (String)jobj.get(VariantRec.GENE_NAME);
-                                        if (geneName != null) break;
-                                    }
-
-				    if (geneName != null) {
+				String geneName = var.getAnnotation(VariantRec.GENE_NAME);
+				if (geneName != null) {
 					if (! genes.containsGene(geneName)) {
 						genes.addGene(geneName);
+						
 					}
-					var.setGene( genes.getGeneByName(geneName) );
-				    }
-                                } catch (JSONException ex) {
-                                    ex.printStackTrace();
-                                    logger.log( Level.WARNING, "GeneListFromVars Error:", ex );
-                                }
+					var.setGene( genes.getGeneByName(geneName));
+				}
 			}
 		}
-		logger.info("Created a gene list with " + genes.size() + " entries from " + vars.size() + " variants");
+		Logger.getLogger(Pipeline.primaryLoggerName).info("Created a gene list with " + genes.size() + " entries from " + vars.size() + " variants");
 	}
 	
 	@Override
