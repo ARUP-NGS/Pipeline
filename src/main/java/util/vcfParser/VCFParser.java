983de7fdb3f8242bb9552dae2edffdf6f0c33598
brendan-ofallon <brendan.o'fallon@aruplab.com>
2017-11-14 13:55:56 -0700

    Add support for orig_ref and orig_alt annos from pindel VCFs

--- src/main/java/util/vcfParser/VCFParser.java
+++ src/main/java/util/vcfParser/VCFParser.java
@@ -545,6 +545,16 @@ public class VCFParser implements VariantLineReader {
 			}
 		}
 
+		String pindelRef = getPindelRef();
+		if (pindelRef != null) {
+			var.addAnnotation(VariantRec.PINDEL_ORIG_REF, pindelRef);
+		}
+
+		String pindelAlt = getPindelAlt();
+		if (pindelAlt != null) {
+			var.addAnnotation(VariantRec.PINDEL_ORIG_ALT, pindelAlt);
+		}
+
 		String genotypeQuality = getGenotypeQuality();
 		if (genotypeQuality != null) {
 			var.addAnnotation(VariantRec.GENOTYPE_QUALITY, genotypeQuality);
@@ -876,8 +886,8 @@ public class VCFParser implements VariantLineReader {
 		} else {
 			return new String[0];
 		}
-	}
-
+	}	
+		
 	/**
 	 * Total read depth at locus from INFO column, identified by "DP" and specified for the particular ALT
 	 * @author elainegee
@@ -1064,6 +1074,28 @@ public class VCFParser implements VariantLineReader {
 	public String getMNPAlleleComponents() {
 		if (sampleMetrics.containsKey("ALLELES")) {
 			return sampleMetrics.get("ALLELES");
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Return the bases associated with the 'pindel.orig.ref' field, or null if that key doesn't exist
+	 */
+	public String getPindelRef() {
+		if (sampleMetrics.containsKey("orig_ref")) {
+			return sampleMetrics.get("orig_ref");
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Return the bases associated with the 'pindel.orig.alt' field, or null if that key doesn't exist
+	 */
+	public String getPindelAlt() {
+		if (sampleMetrics.containsKey("orig_alt")) {
+			return sampleMetrics.get("orig_alt");
 		} else {
 			return null;
 		}
